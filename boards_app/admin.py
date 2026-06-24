f9e4f89b89dcfdcc3b563be9e2cabb01f58d9b45
NadineJuliana <nadine.juliana.b@web.de>
2026-06-24 15:36:37 +0200

    feat(boards): register board model in admin

--- boards_app/admin.py
+++ boards_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Board
 
 # Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "owner", "created_at"]
+    search_fields = ["title", "owner__email", "owner__first_name"]
+    list_filter = ["created_at"]
+    filter_horizontal = ["members"]
