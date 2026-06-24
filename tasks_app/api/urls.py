ef3d30132786a7e02f6f34ca1edeccb0eec49637
NadineJuliana <nadine.juliana.b@web.de>
2026-06-24 18:05:43 +0200

    feat(tasks): implement task endpoints

--- tasks_app/api/urls.py
+++ tasks_app/api/urls.py
@@ -1,3 +1,26 @@
 from django.urls import path
+from .views import (
+    AssignedToMeTaskListView,
+    ReviewingTaskListView,
+    TaskCreateView,
+    TaskDetailView,
+)
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "tasks/assigned-to-me/",
+        AssignedToMeTaskListView.as_view(),
+        name="assigned-tasks",
+    ),
+    path(
+        "tasks/reviewing/",
+        ReviewingTaskListView.as_view(),
+        name="reviewing-tasks",
+    ),
+    path("tasks/", TaskCreateView.as_view(), name="task-create"),
+    path(
+        "tasks/<int:task_id>/",
+        TaskDetailView.as_view(),
+        name="task-detail",
+    ),
+]
