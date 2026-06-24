2513c8ffac9cbcb625b693aa3253230ff1ec5306
NadineJuliana <nadine.juliana.b@web.de>
2026-06-24 17:19:45 +0200

    feat(boards): add board routes

--- boards_app/api/urls.py
+++ boards_app/api/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
+from .views import BoardDetailView, BoardListCreateView, EmailCheckView
 
-urlpatterns = []
+urlpatterns = [
+    path("boards/", BoardListCreateView.as_view(), name="board-list"),
+    path(
+        "boards/<int:board_id>/",
+        BoardDetailView.as_view(),
+        name="board-detail",
+    ),
+    path("email-check/", EmailCheckView.as_view(), name="email-check"),
+]
