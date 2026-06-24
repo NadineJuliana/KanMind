49ccebc5c070fb9d62d1c6c632c7e54068ded62d
NadineJuliana <nadine.juliana.b@web.de>
2026-06-24 17:36:00 +0200

    feat(task): register task model in admin

--- tasks_app/admin.py
+++ tasks_app/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "board",
+        "status",
+        "priority",
+        "assignee",
+        "reviewer",
+        "created_by",
+        "due_date",
+    )
+
+    list_filter = (
+        "status",
+        "priority",
+        "board",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+        "board__title",
+    )
