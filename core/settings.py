a1469369f3b937c21b8e77b485ea5f17720b8c46
NadineJuliana <nadine.juliana.b@web.de>
2026-06-25 12:56:56 +0200

    chore: add cors headers conifuration

--- core/settings.py
+++ core/settings.py
@@ -57,6 +57,16 @@ MIDDLEWARE = [
     
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
+]
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
